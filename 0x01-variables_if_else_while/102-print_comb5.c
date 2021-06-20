308e62f5a37c7c87768f47a19bafdd33d995d0c0
Yusuf Ridwan <horiyorrmi72@gmail.com>
2021-06-20 06:22:20 -0700

    create print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -23,32 +23,32 @@ int main(void)
 	int t;
 	int o;
 
-	for (tens = '0'; tens <= '9'; tens++) /*print first two digit combo*/
+	for (tens = '0'; tens <= '9'; tens++)
 	{
 		for (ones = '0'; ones <= '9'; ones++)
 		{
-		 	for (t = tens; t <= '9'; t++) /*print second of pair*/
+		  for (t = tens; t <= '9'; t++)
 			{
-				for (o = ones + 1; o <= '9'; o++)
+			  for (o = ones + 1; o <= '9'; o++)
+			    {
+			      putchar(tens);
+			      putchar(ones);
+			      putchar(' ');
+			      putchar(t);
+			      putchar(o);
+			      
+			      if (!((tens == '9' && ones == '8') &&
+				    (t == '9' && o == '9')))
 				{
-					putchar(tens);
-					putchar(ones);
-					putchar(' ');
-					putchar(t);
-					putchar(o);
-
-					if (!((tens == '9' && ones == '8') &&
-					      (t == '9' && o == '9')))
-					{
-						putchar(',');
-						putchar(' ');
-					}
+				  putchar(',');
+				  putchar(' ');
 				}
-				o = '0';
+			    }
+			  o = '0';
 			}
 		}
 	}
 	putchar('\n');
-
+	
 	return (0);
 }

43143a338c9a00e2c0c4c9800663edf9404cfcdc
Yusuf Ridwan <horiyorrmi72@gmail.com>
2021-06-20 06:35:21 -0700

    create print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,16 +18,16 @@
  */
 int main(void)
 {
-	int tens;
-	int ones;
-	int t;
-	int o;
+  int tens;
+  int ones;
+  int t;
+  int o;
 
 	for (tens = '0'; tens <= '9'; tens++)
 	{
 		for (ones = '0'; ones <= '9'; ones++)
 		{
-		  for (t = tens; t <= '9'; t++)
+		    for (t = tens; t <= '9'; t++)
 			{
 				for (o = ones + 1; o <= '9'; o++)
 				{

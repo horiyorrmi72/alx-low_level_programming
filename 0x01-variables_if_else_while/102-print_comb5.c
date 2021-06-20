01c717caa5ddbd4b33924391bb0ada2ecc6455ca
Yusuf Ridwan <horiyorrmi72@gmail.com>
2021-06-20 06:26:39 -0700

    create print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -23,32 +23,32 @@ int main(void)
 	int t;
 	int o;
 
-	for (tens = '0'; tens <= '9'; tens++)
+	for (tens = '0'; tens <= '9'; tens++) 
 	{
 		for (ones = '0'; ones <= '9'; ones++)
 		{
-		  for (t = tens; t <= '9'; t++)
+		 	for (t = tens; t <= '9'; t++) 
 			{
-			  for (o = ones + 1; o <= '9'; o++)
-			    {
-			      putchar(tens);
-			      putchar(ones);
-			      putchar(' ');
-			      putchar(t);
-			      putchar(o);
-			      
-			      if (!((tens == '9' && ones == '8') &&
-				    (t == '9' && o == '9')))
+				for (o = ones + 1; o <= '9'; o++)
 				{
-				  putchar(',');
-				  putchar(' ');
+					putchar(tens);
+					putchar(ones);
+					putchar(' ');
+					putchar(t);
+					putchar(o);
+
+					if (!((tens == '9' && ones == '8') &&
+					      (t == '9' && o == '9')))
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
-			    }
-			  o = '0';
+				o = '0';
 			}
 		}
 	}
 	putchar('\n');
-	
+
 	return (0);
 }

359e4ede9a101158e8f32044210d2ef8ef64d58a
Yusuf Ridwan <horiyorrmi72@gmail.com>
2021-06-20 07:44:20 -0700

    create printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Return:0
+ *
+ */
+int main(void)
+{
+  printf("with proper grammer, but the outcome is a piece of art");
+  return (0);
+}

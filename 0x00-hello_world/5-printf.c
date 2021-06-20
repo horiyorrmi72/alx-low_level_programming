e7179d016a077014cd5b48de185c709c41633668
Yusuf Ridwan <horiyorrmi72@gmail.com>
2021-06-20 07:50:43 -0700

    create printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,9 @@
 /**
  * main - main block
  * Return:0
- *
  */
 int main(void)
 {
-  printf("with proper grammer, but the outcome is a piece of art,\n");
+  printf("with proper grammar, but the outcome is a piece of art,\n");
   return (0);
 }

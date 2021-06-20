41311c70a15ad2f6aff3d30492a058a5a65e8f4a
Yusuf Ridwan <horiyorrmi72@gmail.com>
2021-06-20 06:18:22 -0700

    create print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,6 +4,16 @@
  * main - main block
  * Description: Write a program that prints all possible
  * different combinations of two digits
+ * Numbers must be separated by ,, followed by a space
+ * The two digits must be different
+ * 01 and 10 are considered the same combination of the two digits 0 and 1
+ * Print only the smallest combination of two digits
+ * Numbers should be printed in ascending order, with two digits
+ * You can only use the putchar function
+ * (every other function (printf, puts, etc…) is forbidden)
+ * You can only use putchar five times maximum in your code
+ * You are not allowed to use any variable of type char
+ * All your code should be in the main function
  * Return: 0
  */
 int main(void)
@@ -13,12 +23,12 @@ int main(void)
 	int t;
 	int o;
 
-	for (tens = '0'; tens <= '9'; tens++)
+	for (tens = '0'; tens <= '9'; tens++) /*print first two digit combo*/
 	{
 		for (ones = '0'; ones <= '9'; ones++)
 		{
-		 	for (t = tens; t <= '9'; t++)
-			  {
+		 	for (t = tens; t <= '9'; t++) /*print second of pair*/
+			{
 				for (o = ones + 1; o <= '9'; o++)
 				{
 					putchar(tens);

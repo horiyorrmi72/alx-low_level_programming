655aecfd92d1af049b1bef27459bb60af425ecb6
Yusuf Ridwan <horiyorrmi72@gmail.com>
2021-06-20 08:26:03 -0700

    create quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  * Return:1
@@ -7,6 +7,6 @@
 int main(void)
 {
 char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-fwrite(2, ch, sizeof(ch));
+write(2, ch, sizeof(ch));
 return (1);
 }

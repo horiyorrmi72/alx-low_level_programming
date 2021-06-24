2e60155442bd09513e07e2b6a1ae121e5b769a58
Yusuf Ridwan <horiyorrmi72@gmail.com>
2021-06-24 07:55:59 -0700

    create main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *main - test function prints if integer is positive or neagative
+ *Return: 0
+ */
+
+int main(void)
+{
+  int i;
+
+  i=0;
+  positive_or_negative(i);
+
+  return(0);
+}

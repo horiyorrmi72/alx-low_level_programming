71d9e1456965d1362cd54e8f959f451e6c4e4bbe
Yusuf Ridwan <horiyorrmi72@gmail.com>
2021-06-24 09:56:45 -0700

    create 2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - function that multiplies two integers
+ *@f: First integer
+ *@s: Second integer
+ *
+ * Return: Always 0.
+ */
+int mul(int y, int s)
+{
+  int x;
+
+  x = y * s;
+  return (x);
+}

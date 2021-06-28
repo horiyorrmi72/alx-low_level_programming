1842c72500359882f297a0589dc9abfef76dae5c
Yusuf Ridwan <horiyorrmi72@gmail.com>
2021-06-28 09:07:02 -0700

    update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,6 +4,7 @@
 /**
  * main - update to a point to 98
  *
+ * description: function that takes a pointer to an int as parameter and updates the value it points to to 98
  *@n: pointer
  * Return: Always 0.
  */

4f2fa1e620fa0d380a7b81ece40945a347e0d650
Yusuf Ridwan <horiyorrmi72@gmail.com>
2021-06-28 09:24:59 -0700

    create print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * print_rev - prints string in reverse.
+ * @s: value taken from main
+ * Return: (0)
+ */
+void print_rev(char *s)
+{
+	int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+	}
+	for (len = len - 1; len >= 0; len--)
+	{
+		_putchar(s[len]);
+	}
+	_putchar('\n');
+}

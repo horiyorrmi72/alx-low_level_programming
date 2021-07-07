25d8a9c398b0988eed3f1acbb5c30a3f8c1181b3
Yusuf Ridwan <horiyorrmi72@gmail.com>
2021-07-07 03:38:35 -0700

    update

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -1,3 +1,6 @@
+#ifndef HEADER
+#define HEADER
+
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -9,3 +12,5 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);~
 int factorial(int n);~
+
+#endif

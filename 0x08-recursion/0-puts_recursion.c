980804fba4ef91027f1efc99b39c1636878abf47
Yusuf Ridwan <horiyorrmi72@gmail.com>
2021-07-07 05:27:06 -0700

    update

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,18 @@
 #include "holberton.h"
+
 /**
- *_puts_recursion - prints a string, followed by a new line
- *@s: pointer to string
- *Return: (0);
- **/
+ * _puts_recursion - print string with new line
+ * @s: string to print
+ */
+
 void _puts_recursion(char *s)
 {
-if (*s == '\0')
-{
-      _putchar('\n');
-      return;
-    }
-  _putchar(*s);
-  _puts_recursion(s + 1);
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
